539492786d6d3fdb4fba30fa62b077c840c15e45
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2025-02-25 12:32:20 +0100

    search bar with text field

--- app/src/main/java/de/marquisproject/fionotes/MainActivity.kt
+++ app/src/main/java/de/marquisproject/fionotes/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.BottomAppBar
@@ -19,17 +27,29 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import de.marquisproject.fionotes.ui.theme.FionotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,64 +59,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FionotesTheme {
+                var searchText by remember { mutableStateOf("") }
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     bottomBar = {
-                        BottomAppBar(
-                            actions = {
-                                NavigationBarItem(
-                                    icon = {
-                                        Icon(Icons.Outlined.Home, "Localized description")
-                                    },
-                                    label = { Text("Notes") },
-                                    selected = false,
-                                    onClick = { }
-                                )
-                                NavigationBarItem(
-                                    icon = {
-                                        Icon(Icons.Outlined.Delete, "Localized description")
-                                    },
-                                    label = { Text("Bin") },
-                                    selected = false,
-                                    onClick = { }
-                                )
-                                NavigationBarItem(
-                                    icon = {
-                                        Icon(
-                                            painter = painterResource(id = R.drawable.inventory_2_24dp),
-                                            "Localized description")
-                                    },
-                                    label = { Text("Archived") },
-                                    selected = false,
-                                    onClick = { }
-                                )
-                                Spacer(modifier = Modifier.weight(1f))
-                            },
-                            floatingActionButton = {
-                                FloatingActionButton(
-                                    onClick = { /* do something */ },
-                                    containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
-                                    elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
-                                ) {
-                                    Icon(Icons.Filled.Add, "Localized description")
-                                }
-                            }
-                        )
+                        BottomBar()
                     },
                     topBar = {
-                        TopAppBar(
-                            colors = TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary,
-                            ),
-                            title = {
-                                Text("Fionotes")
-                            }
+                        TopBar(
+                            query = searchText,
+                            onQueryChange = { searchText = it },
                         )
                     },
                 ) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = searchText,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -113,19 +90,92 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FAB(onClick: () -> Unit
+fun TopBar(
+    query: String,
+    onQueryChange: (String) -> Unit,
 ) {
-    FloatingActionButton(
-        onClick = { onClick() },
-        containerColor = Color(252, 238, 131,255),
-        contentColor = MaterialTheme.colorScheme.onPrimary,
-        modifier = Modifier
-    ) {
-        Icon(Icons.Filled.Add, "Floating action button.")
-    }
+    TopAppBar(
+        title = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // Search Bar
+                OutlinedTextField(
+                    value = query,
+                    onValueChange = onQueryChange,
+                    placeholder = { Text("Search Fionotes ...") },
+                    modifier = Modifier
+                        .weight(1f), // Makes search bar take up all available space
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
+                    },
+                    singleLine = true,
+                    shape = RoundedCornerShape(8.dp), // Reduce text size
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                // Right-side Icon Button
+                IconButton(
+                    onClick = {
+                        /* handle icon click */
+                    }
+                ) {
+                    Icon(imageVector = Icons.Default.Settings, contentDescription = "More Options")
+                }
+            }
+        }
+    )
 }
 
+@Composable
+fun BottomBar() {
+    BottomAppBar(
+        actions = {
+            NavigationBarItem(
+                icon = {
+                    Icon(Icons.Outlined.Home, "Localized description")
+                },
+                label = { Text("Notes") },
+                selected = false,
+                onClick = { }
+            )
+            NavigationBarItem(
+                icon = {
+                    Icon(Icons.Outlined.Delete, "Localized description")
+                },
+                label = { Text("Bin") },
+                selected = false,
+                onClick = { }
+            )
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.inventory_2_24dp),
+                        "Localized description")
+                },
+                label = { Text("Archived") },
+                selected = false,
+                onClick = { }
+            )
+            Spacer(modifier = Modifier.weight(1f))
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* do something */ },
+                containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+            ) {
+                Icon(Icons.Filled.Add, "Localized description")
+            }
+        }
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
