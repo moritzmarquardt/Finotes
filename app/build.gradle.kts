fa376412d6a6616e6c896ba2b8a6d7c26956102c
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2025-08-10 23:09:52 +0200

    release: version 1.0.1 signed apk and app bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "de.marquisproject.finotes"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "v1.0.0"
+        versionCode = 4
+        versionName = "v1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
